4489671b47c5f03702912ffdb0454d05dedd541b
rishabh911996 <rishabh.911996@gmail.com>
2022-01-19 21:12:53 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is a new update !!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <p className='App-intro'>
+        Bye There!!!
+      </p>
+    </div>
+  );
+}
+
+export default App;
