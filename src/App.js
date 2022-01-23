0feb2a7aeb21a0d0e872137c61c26dfc671f4ff5
rishabh911996 <rishabh.911996@gmail.com>
2022-01-22 17:40:00 -0700

    added updated travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a new update test update from the Web!!!
+          This is a new update test update from the Web 1234!!!
         </p>
         <a
           className="App-link"
